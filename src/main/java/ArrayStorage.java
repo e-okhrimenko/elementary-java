45201084d7ca369f53e03e9a776811e4838e5ef2
Evhen <oeasoft@gmail.com>
2021-03-11 21:16:02 +0200

    [HW-04] Fixed implementation of methods.

--- src/main/java/ArrayStorage.java
+++ src/main/java/ArrayStorage.java
@@ -1,84 +1,58 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Employees
  */
 public class ArrayStorage {
     Employee[] storage = new Employee[10000];
+    private int size;
 
     void clear() {
-        for (int i = 0; i < size(); i++) {
-            storage[i] = null;
-        }
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
     }
 
     void save(Employee employee) {
-        if (storage.length == size()) {
+        if (storage.length == size) {
             System.out.println("The array is full. Employee: " + employee + ", not added.");
             return;
         }
-        storage[size()] = employee;
+        if (employee == null) {
+            System.out.println("Not added. Employee parameter is null.");
+            return;
+        }
+        storage[size] = employee;
+        size++;
     }
 
     Employee get(String uuid) {
-        for (int i = 0; i < size(); i++)
+        for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) {
                 return storage[i];
             }
+        }
         System.out.println("Employee " + uuid + " - not found!");
         return null;
     }
 
     void delete(String uuid) {
-        //Варивнт с переносом крайнего, на место удаленного.
-        for (int i = 0; i < size(); i++) {
+        for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) {
-                storage[i] = storage[size() - 1];
+                storage[i] = storage[size - 1];
                 storage[size() - 1] = null;
+                size--;
             }
         }
-
-        //Варивнт со смещением всех последующих сотрудников методом arraycopy.
-        /*for (int i = 0; i < size(); i++) {
-            if (storage[i].uuid.equals(uuid)) {
-                System.arraycopy(storage, i + 1, storage, i, size() - 1);
-                storage[size() - 1] = null;
-                return;
-            }
-        }*/
-
-        //Варивнт со смещением всех последующих сотрудников.
-        /*for (int i = 0; i < size(); i++) {
-            if (storage[i].uuid.equals(uuid)) {
-                while (i < (size() - 1)) {
-                    storage[i] = storage[i + 1];
-                    i++;
-                }
-                storage[size() - 1] = null;
-                return;
-            }
-        }*/
     }
 
     /**
      * @return array, contains only Employees in storage (without null)
      */
     Employee[] getAll() {
-        Employee[] getAll = new Employee[size()];
-        if (size() != 0) {
-            for (int i = 0; i < size(); i++) {
-                getAll[i] = storage[i];
-            }
-        } else {
-            System.out.println("Employees are missing!");
-        }
-        return getAll;
+        return Arrays.copyOfRange(storage, 0, size);
     }
 
     int size() {
-        int size = 0;
-        for (int i = 0; i < storage.length; i++)
-            if (storage[i] != null) {
-                size++;
-            } else break;
         return size;
     }
 }
