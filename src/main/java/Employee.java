38ce710c4834d2612c82ab14405e3db6edc0c0e1
Evhen <oeasoft@gmail.com>
2021-03-14 21:52:52 +0200

    [HW-05] Updated. Made the uuid field private.

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -4,18 +4,22 @@
 public class Employee {
 
     // Unique identifier
-    private String uuid;
+    private final String uuid;
 
-    public void setUuid(String uuid) {
+    public Employee(String uuid) {
         this.uuid = uuid;
     }
 
-    public String getUuid() {
+    public String getUuid(String uuid) {
         return uuid;
     }
 
     @Override
     public String toString() {
+        return uuid;
+    }
+
+    public String getUuid() {
         return uuid;
     }
 }
