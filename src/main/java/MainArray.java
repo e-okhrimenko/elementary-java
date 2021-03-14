86022a2a04114d8c3873b20dbc150d1a968e2117
Evhen <oeasoft@gmail.com>
2021-03-14 17:03:20 +0200

    [HW-05] Created the MainArray class

---
+++ src/main/java/MainArray.java
@@ -0,0 +1,95 @@
+import java.util.Scanner;
+
+public class MainArray {
+    static final ArrayStorage ARRAY_STORAGE = new ArrayStorage();
+
+    public static void main(String[] args) {
+        System.out.print("\nHello!");
+        Scanner scanner = new Scanner(System.in);
+        boolean repeat = true;
+        while (repeat) {
+            Employee employee = new Employee();
+            System.out.print("\n(list | save uuid | delete uuid | get uuid | size | clear | exit)" + "\n"
+                    + "Enter one of the commands: ");
+            String stringIn = scanner.nextLine();
+            String[] result = stringIn.split("\\s", 2);
+            if (result[0] != null) {
+                result[0] = result[0].toLowerCase();
+            }
+            if (result.length == 1) {
+                assert result[0] != null;
+                if (result[0].equals("save") || result[0].equals("delete")
+                        || result[0].equals("get")) {
+                    System.out.println("The UUID not entered!");
+                    continue;
+                }
+            }
+            if (result.length == 2) {
+                char[] temp = result[1].toCharArray();
+                int wTemp = 0;
+                for (int i = 0; i < temp.length; i++) {
+                    if (!Character.isWhitespace(result[1].charAt(i))) {
+                        result[1] = result[1].trim();
+                        wTemp = 1;
+                        break;
+                    }
+                }
+                if (wTemp != 1) {
+                    System.out.println("Entered incorrectly command or UUID!");
+                    continue;
+                }
+            }
+            if (result[0] != null) {
+                switch (result[0]) {
+                    case "list":
+                        printAll();
+                        break;
+                    case "save":
+                        employee.setUuid(result[1]);
+                        ARRAY_STORAGE.save(employee);
+                        printAll();
+                        break;
+                    case "delete":
+                        ARRAY_STORAGE.delete(result[1]);
+                        printAll();
+                        break;
+                    case "get":
+                        if (ARRAY_STORAGE.get(result[1]) == null) {
+                            System.out.println("Employee " + result[1] + " - not found!");
+                        } else {
+                            System.out.println("Employees " + result[1] + ": " + ARRAY_STORAGE.get(result[1]));
+                        }
+                        break;
+                    case "size":
+                        if (ARRAY_STORAGE.size() > 0) {
+                            System.out.println("Added and stored: " + ARRAY_STORAGE.size() + " employees.");
+                        } else {
+                            System.out.println("No employees added. The storage is empty.");
+                        }
+                        break;
+                    case "clear":
+                        ARRAY_STORAGE.clear();
+                        printAll();
+                        break;
+                    case "exit":
+                        repeat = false;
+                        System.out.println("Exiting ... Goodbye.");
+                        break;
+                    default:
+                        System.out.println("The command is missing or entered incorrectly!");
+                }
+            }
+        }
+    }
+
+    static void printAll() {
+        System.out.println("List of employees (UUID): ");
+        if ((ARRAY_STORAGE.size() == 0)) {
+            System.out.println("No employees.");
+        } else {
+            for (Employee employee : ARRAY_STORAGE.getAll()) {
+                System.out.println(employee);
+            }
+        }
+    }
+}
